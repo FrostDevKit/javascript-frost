b4a1b05a76d7217ba6380d79ef20e1d2b63d95de
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-02 14:10:07 -0300

    Update btc_address.jsx

--- src/btc_address.jsx
+++ src/btc_address.jsx
@@ -1,2 +1,2 @@
 import {Key, Signer, Partires, Index, Pair, Schnorr, Rand } from 'bitcoinjs-lib';
-import {Seperator, fullDerivationPath, partialKeyDerivationPath, deriveChildPublicKey, networkData } from 'xpub-tools';
+import {Seperator, FullDerivationPath, PartialKeyDerivationPath, DeriveChildPublicKey, NetworkData } from 'xpub-tools';
