6a9af2843858b8a2cb78c52646703e301beeb9ff
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-11 14:33:55 -0300

    Update kp256.jsx

--- src/bitcoin/kp256.jsx
+++ src/bitcoin/kp256.jsx
@@ -1,1 +1,35 @@
+// Import necessary modules and libraries
+const { Ciphersuite } = require('ciphersuite');
+
+// Define a generic Curve interface
+class Curve {
+  static get CONTEXT() {
+    return Buffer.from("FROST-P256-SHA256-v1");
+  }
+
+  static hash_to_F(chal, data) {
+    // Implement hash_to_F logic here based on your requirements
+    // Example: return some_hash_function(Buffer.concat([chal, data]));
+  }
+}
+
+// Define the challenge function for P256 ciphersuite
+class IetfP256Hram {
+  static hram(R, A, m) {
+    return Curve.hash_to_F(Buffer.from("chal"), Buffer.concat([R.to_bytes(), A.to_bytes(), m]));
+  }
+}
+
+// Define the challenge function for Secp256k1 ciphersuite
+class IetfSecp256k1Hram {
+  static hram(R, A, m) {
+    return Curve.hash_to_F(Buffer.from("chal"), Buffer.concat([R.to_bytes(), A.to_bytes(), m]));
+  }
+}
+
+module.exports = {
+  IetfP256Hram,
+  IetfSecp256k1Hram,
+  Curve
+};
 
