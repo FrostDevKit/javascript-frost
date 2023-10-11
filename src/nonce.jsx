5b02c7c56ef3f8e77e829e35456a5eb8f0f7a198
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-10-11 14:38:43 -0300

    Update nonce.jsx

--- src/nonce.jsx
+++ src/nonce.jsx
@@ -1,1 +1,211 @@
+const { transcript, hash_binding_factor, multiexp_vartime } = require('your-transcript-lib'); // Replace with actual import
+
+class Nonce {
+  constructor(f) {
+    this.f = f;
+  }
+}
+
+class GeneratorCommitments {
+  constructor(g) {
+    this.g = g;
+  }
+
+  static read(reader) {
+    // Implement read logic
+  }
+
+  write(writer) {
+    // Implement write logic
+  }
+}
+
+class NonceCommitments {
+  constructor(generators) {
+    this.generators = generators;
+  }
+
+  static new(rng, secretShare, generators) {
+    const nonce = new Nonce([
+      randomNonce(secretShare, rng),
+      randomNonce(secretShare, rng),
+    ]);
+
+    const commitments = generators.map((generator) => {
+      return new GeneratorCommitments([
+        generator.multiply(nonce.f[0]),
+        generator.multiply(nonce.f[1]),
+      ]);
+    });
+
+    return [nonce, new NonceCommitments(commitments)];
+  }
+
+  static read(reader, generators) {
+    // Implement read logic
+  }
+
+  write(writer) {
+    // Implement write logic
+  }
+
+  transcript(t) {
+    t.domainSeparate('nonce');
+    this.generators.forEach((commitments) => {
+      t.appendMessage('commitment_D', commitments.g[0].toBytes());
+      t.appendMessage('commitment_E', commitments.g[1].toBytes());
+    });
+  }
+
+  aggregationFactor(context) {
+    const transcript = aggregationTranscript(context);
+    this.transcript(transcript);
+    return hash_binding_factor('dleq_aggregation', transcript.challenge('binding'));
+  }
+}
+
+class Commitments {
+  constructor(nonces, dleq) {
+    this.nonces = nonces;
+    this.dleq = dleq;
+  }
+
+  static new(rng, secretShare, plannedNonces, context) {
+    const nonces = [];
+    const commitments = [];
+
+    const dleqGenerators = [];
+    const dleqNonces = [];
+
+    plannedNonces.forEach((generators) => {
+      const [nonce, theseCommitments] = NonceCommitments.new(rng, secretShare, generators);
+
+      if (generators.length > 1) {
+        dleqGenerators.push(generators);
+        dleqNonces.push(
+          new Zeroizing(
+            theseCommitments.aggregationFactor(context).multiply(nonce.f[1]).add(nonce.f[0])
+          )
+        );
+      }
+
+      nonces.push(nonce);
+      commitments.push(theseCommitments);
+    });
+
+    let dleq = null;
+
+    if (dleqGenerators.length > 0) {
+      dleq = MultiDLEqProof.prove(rng, dleqTranscript(context), dleqGenerators, dleqNonces);
+      const verifyResult = dleq.verify(dleqTranscript(context), dleqGenerators, dleqNonces);
+      if (!verifyResult) {
+        throw new Error('Invalid DLEq proof');
+      }
+    }
+
+    return [nonces, new Commitments(commitments, dleq)];
+  }
+
+  transcript(t) {
+    t.domainSeparate('commitments');
+    this.nonces.forEach((nonce) => {
+      nonce.transcript(t);
+    });
+
+    if (this.dleq) {
+      t.appendMessage('dleq', this.dleq.serialize());
+    }
+  }
+
+  static read(reader, generators, context) {
+    // Implement read logic
+  }
+
+  write(writer) {
+    // Implement write logic
+  }
+}
+
+class IndividualBinding {
+  constructor(commitments) {
+    this.commitments = commitments;
+    this.bindingFactors = null;
+  }
+}
+
+class BindingFactor {
+  constructor() {
+    this.participants = new Map();
+  }
+
+  insert(i, commitments) {
+    this.participants.set(i, new IndividualBinding(commitments));
+  }
+
+  calculateBindingFactors(transcript) {
+    this.participants.forEach((binding, l) => {
+      const clonedTranscript = transcript.clone();
+      clonedTranscript.appendMessage('participant', l.toString());
+      binding.bindingFactors = [];
+      binding.commitments.nonces.forEach((nonce, index) => {
+        binding.bindingFactors.push(hash_binding_factor('rho', clonedTranscript.challenge('rho')));
+      });
+    });
+  }
+
+  bindingFactors(i) {
+    return this.participants.get(i).bindingFactors;
+  }
+
+  bound(l) {
+    const res = [];
+    this.participants.get(l).commitments.nonces.forEach((nonce, index) => {
+      res.push([]);
+      nonce.generators.forEach((generator) => {
+        const bindingFactor = this.bindingFactors(l)[index];
+        res[index].push(generator.g[0].add(generator.g[1].multiply(bindingFactor)));
+      });
+    });
+    return res;
+  }
+
+  nonces(plannedNonces) {
+    const nonces = [];
+    plannedNonces.forEach((generators) => {
+      const plannedNoncesForGenerator = [];
+      this.participants.forEach((binding) => {
+        const statements = binding.commitments.nonces.map((nonce) => {
+          return [this.bindingFactors(binding).map((f, index) => [f, nonce.generators[index].g[1]])];
+        });
+        const combined = multiexp_vartime(statements, this.bindingFactors(binding).map((f) => [f, generators[0]]));
+        plannedNoncesForGenerator.push(generators[0].g[0].add(combined));
+      });
+      nonces.push(plannedNoncesForGenerator);
+    });
+    return nonces;
+  }
+}
+
+// Implementation of other missing functions goes here
+
+// Example usage
+const plannedNonces = [[generator1, generator2], [generator3]];
+const binding = new BindingFactor();
+
+const secretShare = new Zeroizing(/* Secret share data */);
+const rng = /* Your RNG instance */;
+
+const [nonces, commitments] = Commitments.new(rng, secretShare, plannedNonces, context);
+binding.insert(participant, commitments);
+
+// Calculate binding factors for participants
+const transcript = /* Transcript instance */;
+binding.calculateBindingFactors(transcript);
+
+// Get bound nonces for a specific participant
+const l = /* Participant */;
+const boundNonces = binding.bound(l);
+
+// Get nonces for the signing session
+const noncesForSession = binding.nonces(plannedNonces);
 
