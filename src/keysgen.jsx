bb49de2d6fe0fef953d1bb931cd90f6c72f177b8
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-11-21 06:17:06 -0300

    Update keysgen.jsx

--- src/keysgen.jsx
+++ src/keysgen.jsx
@@ -4,8 +4,8 @@ import { secp256k1 } from 'eciesjs';
 export const secp256k1
 export const ellipticurve
 
-const commitShare = {
-pub commitment: Vec<RistrettoPoint>;
+const commitShare = { 
+pub ristrettoPoint: String;
 pub commitment: String;
   
 const KeyGenCommit = {
