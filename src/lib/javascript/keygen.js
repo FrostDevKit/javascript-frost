fad7b22b02c500efecfbe366ee63236674d14e0d
223880 <rafael.segala45@gmail.com>
2024-06-21 07:53:55 -0300

    Fix

--- src/lib/javascript/keygen.js
+++ src/lib/javascript/keygen.js
@@ -1,11 +1,38 @@
 const {Keys,PrivateKey,PublicKey} = require('bitcoinjs-lib');
 
 const key = new PrivateKey();
-const publicKey = key.toPublicKey();
-const address = publicKey.toAddress();
+const PublicKey = key.toPublicKey();
+const Address = publicKey.toAddress();
 const keys = Keys.fromPrivateKey(key);
 const publicKey = keys.publicKey;
 const privateKey = keys.toWIF();
+
+const {Address} = bitcoin.payments.p2pkh({pubkey: publicKey});
+const {address} = bitcoin.payments.p2wpkh({pubkey: publicKey});
+const {address} = bitcoin.payments.p2wpkh({pubkey: publicKey, network: bitcoin.networks.testnet});
+const {address} = bitcoin.payments.p2wpkh({pubkey: publicKey, network: bitcoin.networks.regtest});
+const {address} = bitcoin.payments.p2ptr({pubkey: publicKey, network: bitcoin.networks.testnet});
+
+const Keys = {
+    fromPrivateKey: (privateKey) => {
+        return new Keys(privateKey);
+    },
+    fromExtendedPrivateKey: (extendedPrivateKey) => {
+        return new Keys(extendedPrivateKey);
+    },
+    fromExtendedPublicKey: (extendedPublicKey) => {
+        return new Keys(extendedPublicKey);
+    },
+    fromPublicKey: (publicKey) => {
+        return new Keys(publicKey);
+    },
+    fromWIF: (wif) => {
+        return new Keys(wif);
+    },
+    fromString: (str) => {
+        return new Keys(str);
+    },
+}
 
 console.log(address.toString());
 console.log(publicKey.toString('hex'));
