1989d5cf152eacdbea6f8d150d6f56106c1bac71
223880 <rafael.segala45@gmail.com>
2024-06-27 11:30:07 -0300

    Fix

---
+++ src/bitcoind/index.js
@@ -0,0 +1,6 @@
+const config = require('./config.json');
+
+// Accessing network URLs
+console.log('Mainnet URL:', config.network.mainnet);
+console.log('Testnet4 URL:', config.network.testnet4);
+console.log('Signet URL:', config.network.signet);
