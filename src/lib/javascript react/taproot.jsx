597889fd7b2beab5fd174aac79e187ec7b25d029
223880 <rafael.segala45@gmail.com>
2024-07-10 06:50:00 -0300

    Add taproot

--- src/lib/javascript react/taproot.jsx
+++ src/lib/javascript react/taproot.jsx
@@ -1,1 +1,47 @@
-const {UTXO, Payment, P2TR} from 'bitcoinjs-lib';
+const {UTXO, Payment, P2TR} = require('bitcoinjs-lib');
+
+const {
+  P2TR: {
+    Taproot,
+    Tapscript,
+  },
+} = require('bitcoinjs-lib');
+
+const {
+  P2TR: {
+    Taproot: {
+      TapBranch,
+      TapLeaf,
+    },
+  },
+} = require('bitcoinjs-lib');
+
+const {
+  P2TR: {
+    Taproot: {
+      TapBranch: {
+        TapTweak,
+      },
+    },
+  },
+} = require('bitcoinjs-lib');
+
+const {
+  P2TR: {
+    Taproot: {
+      TapBranch: {
+        TapTweak: {
+          TapTweakAdd,
+          TapTweakMul,
+        },
+      },
+    },
+  },
+} = require('bitcoinjs-lib');
+
+const { 
+ UTXO
+ UTXO.fromObject
+ UTXO.fromBuffer
+ UTXO.fromHex
+} = require('bitcoinjs-lib');
