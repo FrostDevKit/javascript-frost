f4f198b5138e0c1bba28697bd826c7db0bbaf193
223880 <rafael.segala45@gmail.com>
2024-08-19 07:54:12 -0300

    Fix

--- src/lib/javascript/keygen.js
+++ src/lib/javascript/keygen.js
@@ -2,8 +2,7 @@ const {Keys,PrivateKey,PublicKey} = require('bitcoinjs-lib');
 
 const key = new PrivateKey();
 const PublicKey = key.toPublicKey();
-const Address = publicKey.toAddress();
-const keys = Keys.fromPrivateKey(key);
+const address = publicKey.toAddress();const keys = Keys.fromPrivateKey(key);
 const publicKey = keys.publicKey;
 const privateKey = keys.toWIF();
 
