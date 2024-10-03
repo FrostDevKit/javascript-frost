9c40b6c0850fbf4ebdc9c2118ad08b5bb8c4ade5
223880 <rafael.segala45@gmail.com>
2024-10-03 07:04:22 -0300

    Initial PoC to BIP

--- src/bip/poc.jsx
+++ src/bip/poc.jsx
@@ -1,1 +1,22 @@
-import
+import { TinySecp256k1Interface } from "bitcoinjs-lib/src/types";
+import { Secp256k1Interface } from "bitcoinjs-lib/src/interfaces/ecpair";
+
+export const NETWORK = {
+  name: "testnet",
+  messagePrefix: "\x18Bitcoin Signed Message:\n",
+  bech32: "bc",
+  bip32: {
+    public: 0x043587cf,
+    private: 0x04358394,
+  },
+  pubKeyHash: 0x6f,
+  scriptHash: 0xc4,
+  wif: 0xef,
+
+};
+
+export const Secp256k1Interface: Secp256k1Interface = new TinySecp256k1Interface();
+
+export const TinySecp256k1Interface = Secp256k1Interface;
+
+
