de714aeb338e2f76fa6ce259b3eb64da05bd120f
22388o <22388o@users.noreply.github.com>
2024-12-02 07:41:06 -0300

    Fix index

--- src/lib/javascript/index.js
+++ src/lib/javascript/index.js
@@ -24,12 +24,12 @@ describe('BitcoinJS-Lib', () => {
   it('should get address from public key', () => {
     const publicKey = new PublicKey(Buffer.from('0311db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5c', 'hex'));
     const address = getAddressFromPublicKey(publicKey);
-    expect(address).toBe('111111111111111111114
-        oLvT2');
-        expect(isValidAddress(address)).toBe(true);
+    expect(address).toBe('111111111111111111114oLvT2');
+    expect(isValidAddress(address)).toBe(true);
   });
-
-  it('should get address from private key', () => {
-    const privateKey = new PrivateKey(Buffer.from('0000000000000000000000000000000000000000000000000000000000000
-        000', 'hex'));
-        const address = getAddressFromPrivateKey(privateKey);
+    it('should get address from private key', () => {
+      const privateKey = new PrivateKey(Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', 'hex'));
+      const address = getAddressFromPrivateKey(privateKey);
+      expect(address).toBe('111111111111111111114oLvT2');
+    });
+}
