67a25137a4a4249d8235d47a2ae72187cc37d7da
22388o <22388o@users.noreply.github.com>
2025-01-13 06:41:52 -0300

    Fix lightning

--- src/lib/javascript react/lightning.jsx
+++ src/lib/javascript react/lightning.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Lightning = () => {
-  return (
-    <div>
-      <h1>Lightning</h1>
-      {/* Add your Lightning component content here */}
-    </div>
-  );
-};
+export const Lightning = {
+  htlc: function(props) {
+    return (
+      <div className="lightning">
+        <div className="lightning-bolt"></div>
+        <div className="lightning-bolt"></div>
+        <div className="lightning-bolt"></div>
+      </div>
+    );
+  }
+}
