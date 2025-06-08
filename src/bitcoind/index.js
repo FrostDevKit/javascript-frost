9daa16f3047310438692bdb7fc5f0cfe64686c19
223880⚡️ <83122757+22388o@users.noreply.github.com>
2025-06-08 12:02:14 -0300

    Update index.js

--- src/bitcoind/index.js
+++ src/bitcoind/index.js
@@ -1,7 +1,6 @@
-{
-  "network": {
-    "mainnet": "http://yourrpcuser:yourstrongpassword@127.0.0.1:8332",
-    "testnet4": "http://yourrpcuser:yourstrongpassword@127.0.0.1:18332",
-    "signet": "http://yourrpcuser:yourstrongpassword@127.0.0.1:38332"
-  }
-}
+const config = require('./config.json');
+
+// Accessing network URLs
+console.log('Mainnet URL:', config.network.mainnet);
+console.log('Testnet4 URL:', config.network.testnet4);
+console.log('Signet URL:', config.network.signet);
